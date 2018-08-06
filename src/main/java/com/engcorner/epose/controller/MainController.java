fbe3a5791a29d3b9a5eee52c6347abd77403d5d5
cacacai <jiashengcai97@gmail.com>
2018-08-06 15:29:40 +0800

    做了点选课，发送json到前端，后台还是收不到json

--- src/main/java/com/engcorner/epose/controller/MainController.java
+++ src/main/java/com/engcorner/epose/controller/MainController.java
@@ -7,8 +7,12 @@ import com.engcorner.epose.domain.user.UserPose;
 import com.engcorner.epose.repository.course.CourseRepository;
 import com.engcorner.epose.repository.user.UserPoseRepository;
 import com.engcorner.epose.repository.user.UserRepository;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.catalina.connector.Response;
 import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,10 +20,7 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,7 +28,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController implements WebMvcConfigurer {
@@ -61,22 +64,22 @@ public class MainController implements WebMvcConfigurer {
         } else {
             model.addAttribute("lastCourse", courseRepository.findById(courseid).get());
         }
-        model.addAttribute("recommendCourse",null);//推荐课程
+        model.addAttribute("recommendCourse", null);//推荐课程
         model.addAttribute("currentAction", user.getUserPose().getCurrentAction());
         model.addAttribute("courseList", courseRepository.findAll());
         return "classroom";
     }
 
     @RequestMapping(value = {"/test"})
-    public String test(Model model, HttpServletRequest request) {
-        return "action1-1";
+    public String test(Model model) {
+        return "test";
     }
 
     @GetMapping("/course")
     public String course(Model model, HttpServletRequest request,
                          @RequestParam(value = "actionid", defaultValue = "1") String actionid) {
         String courseid = request.getParameter("courseid");//获取请求动作id
-        if (courseid == null){
+        if (courseid == null) {
             return "classroom";
         }
         Course curcourse = courseRepository.findById(Long.parseLong(courseid)).get();
@@ -85,8 +88,8 @@ public class MainController implements WebMvcConfigurer {
         List<Action> actions = curcourse.getActions();
         for (Action action :
                 actions) {
-            if (Long.parseLong(courseid) == action.getId()){
-                currentAction=action;
+            if (Long.parseLong(courseid) == action.getId()) {
+                currentAction = action;
                 break;
             }
         }
@@ -158,9 +161,33 @@ public class MainController implements WebMvcConfigurer {
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
+        if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login";
     }
+
+    @GetMapping(value = "sendAjax")
+    @ResponseBody
+    public String sendAjax(@RequestParam(value = "name",defaultValue = "test") String name){
+        System.out.println(name);
+        return "Done";
+    }
+
+
+
+
+    //把接送数据传输到前端页面
+    @GetMapping(value = "/getAjax")
+    @ResponseBody
+    public Map<String, String> getAjax(){
+        Map<String, String> map= new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "4");
+        return map;
+    }
+
+
+
 }
+
